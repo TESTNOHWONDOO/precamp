586063f949bd32a0b039f6fc52f5f955ad272f89
TESTNOHWONDOO <studycloudera01@gmail.com>
2021-05-07 17:26:57 +0900

    나의 첫 커밋

---
+++ class/03-javascript/02-for.js
@@ -0,0 +1,15 @@
+for(let i=0 ; i<5 ; i=i+1){
+    console.log("안녕하세요.")
+}
+
+for(let i=0 ; i<5 ; i++){
+    console.log(i + "번째입니다.")
+}
+
+const classmates = ["철수", "영희", "훈이"]
+for(let i=0 ; i<classmates.length ; i++) {
+    console.log(classmates[i] + "입니다~")
+}
+
+
+
